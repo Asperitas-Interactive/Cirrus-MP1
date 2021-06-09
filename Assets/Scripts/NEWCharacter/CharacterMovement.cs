242e77486ab4ead5c37b6186a206d2ece1ed8fd5
Pikaboi <braden.cleur@gmail.com>
2021-06-09 15:32:55 +1200

    ah

--- Assets/Scripts/NEWCharacter/CharacterMovement.cs
+++ Assets/Scripts/NEWCharacter/CharacterMovement.cs
@@ -67,9 +67,9 @@ public class CharacterMovement : MonoBehaviour
             m_speed = m_baseSpeed;
         }
         
-        if (movement.magnitude > 0.1f)
+        if (movementRaw.magnitude > 0.1f)
         {
-            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + m_playerCam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(movementRaw.x, movementRaw.z) * Mathf.Rad2Deg + m_playerCam.eulerAngles.y;
             transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             m_controller.Move(moveDir.normalized * (m_speed * Time.deltaTime));
