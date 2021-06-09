f193a48c456439e7aea72087123b7d3665b9f2c3
thunderChawla <58020143+himchawla@users.noreply.github.com>
2021-06-09 15:21:28 +1200

    FirstCommit, SourceTree

--- Assets/Scripts/NEWCharacter/CharacterMovement.cs
+++ Assets/Scripts/NEWCharacter/CharacterMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -23,6 +23,8 @@ public class CharacterMovement : MonoBehaviour
     private bool m_isGrounded = false;
     [SerializeField]
     private float m_gravity = 20.0f;
+    [SerializeField] 
+    private Transform m_playerCam;
 
     //Axis
     [SerializeField]
@@ -31,7 +33,7 @@ public class CharacterMovement : MonoBehaviour
     private float m_VelZ;
 
     private Vector3 Velocity;
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -48,19 +50,25 @@ public class CharacterMovement : MonoBehaviour
 
         Vector3 movement = new Vector3(m_VelX, 0.0f, m_VelZ);
 
-        m_controller.Move(movement * m_speed * Time.deltaTime);
+        if (movement.magnitude > 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + m_playerCam.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            m_controller.Move(moveDir.normalized * (m_speed * Time.deltaTime));
+        }
 
         if (m_isGrounded)
         {
             Velocity = Vector3.zero;
         }
 
-        if (Input.GetButton("Jump") && m_isGrounded)
+        if (Input.GetButtonDown("Jump") && m_isGrounded)
         {
             Velocity.y += m_jumpheight;
-        } else
+        } else if(!m_isGrounded)
         {
-            Velocity.y += m_gravity;
+            Velocity.y += m_gravity * Time.deltaTime;
         }
 
         m_controller.Move(Velocity * Time.deltaTime);
