4a638b8b030ec6847a2e07501619d7a863912adf
Pikaboi <braden.cleur@gmail.com>
2021-06-16 16:23:59 +1200

    refactor

--- Assets/Scripts/NEWCharacter/CharacterMovement.cs
+++ Assets/Scripts/NEWCharacter/CharacterMovement.cs
@@ -57,46 +57,72 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetAxis();
+
+        m_isGrounded = Physics.Raycast(transform.position, -Vector3.up, out hitInfo, m_groundDistance, m_layerMask);
+
+        SpeedControl();
+
+        GlideControl();
+
+        MovementControl();
+
+        ApplyGravity();
+    }
+
+    void GetAxis()
+    {
         m_VelX = Input.GetAxisRaw("Horizontal");
         m_VelZ = Input.GetAxisRaw("Vertical");
         m_smoothX = Input.GetAxis("Horizontal");
         m_smoothZ = Input.GetAxis("Vertical");
 
         movementRaw = new Vector3(m_VelX, 0.0f, m_VelZ);
+    }
 
-        m_isGrounded = Physics.Raycast(transform.position, -Vector3.up, out hitInfo, m_groundDistance, m_layerMask);
-
+    void SpeedControl()
+    {
         if (Input.GetButton("Sprint"))
         {
             m_speed = m_sprintSpeed;
-        } else
+        }
+        else
         {
             m_speed = m_baseSpeed;
         }
 
         if (m_isGrounded) //Whenever player is grounded
         {
-            isGliding = false;
             Velocity = Vector3.zero;
             movementSmooth = new Vector3(m_smoothX, 0.0f, m_smoothZ);
             movementAir = m_speed;
             m_freeLook.m_XAxis.m_MaxSpeed = 450;
-
         }
+    }
 
-        if(!m_isGrounded && Input.GetButtonDown("Jump"))
+    void GlideControl()
+    {
+        if (!m_isGrounded && Input.GetButtonDown("Jump"))
         {
             isGliding = true;
             //No movement Airspeed while gliding
             movementAir = 0;
         }
-        if(!m_isGrounded && Input.GetButtonUp("Jump"))
+        if (!m_isGrounded && Input.GetButtonUp("Jump"))
         {
             isGliding = false;
             //Return to base speed on release
             movementAir = m_baseSpeed;
         }
 
+        if (m_isGrounded)
+        {
+            isGliding = false;
+        }
+    }
+
+   void MovementControl()
+    {
         if (m_isGrounded && movementRaw.magnitude > 0.1f) //If grounded and moving
         {
             float targetAngle = Mathf.Atan2(movementRaw.x, movementRaw.z) * Mathf.Rad2Deg + m_playerCam.eulerAngles.y;
@@ -104,7 +130,8 @@ public class CharacterMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             m_controller.Move(moveDir.normalized * (m_speed * Time.deltaTime));
 
-        } else if (!m_isGrounded && movementSmooth.magnitude > 0.1f && !isGliding) //If in the air and moving
+        }
+        else if (!m_isGrounded && movementSmooth.magnitude > 0.1f && !isGliding) //If in the air and moving
         {
             float targetAngle = Mathf.Atan2(movementSmooth.x, movementSmooth.z) * Mathf.Rad2Deg + m_playerCam.eulerAngles.y;
 
@@ -112,11 +139,13 @@ public class CharacterMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             m_controller.Move(moveDir * (movementAir * Time.deltaTime));
 
-        } else if (!m_isGrounded && !isGliding)//If in the air and not moving / gliding
+        }
+        else if (!m_isGrounded && !isGliding)//If in the air and not moving / gliding
         {
             m_controller.Move(movementSmooth * (movementAir * Time.deltaTime));
 
-        } else if (!m_isGrounded && isGliding && movementRaw.magnitude > 0.1f) //if gliding while moving
+        }
+        else if (!m_isGrounded && isGliding && movementRaw.magnitude > 0.1f) //if gliding while moving
         {
             //Update smooth for when we release
             movementSmooth = new Vector3(m_smoothX, 0.0f, m_smoothZ);
@@ -126,35 +155,37 @@ public class CharacterMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             m_controller.Move(moveDir * (m_baseSpeed * Time.deltaTime));
 
-        } else if(!m_isGrounded && isGliding){ //If gliding without moving
+        }
+        else if (!m_isGrounded && isGliding)
+        { //If gliding without moving
             //Update Smooth so we keep the speed of gliding when we let release
             movementSmooth = new Vector3(m_smoothX, 0.0f, m_smoothZ);
             m_controller.Move(movementSmooth * (movementAir * Time.deltaTime));
         }
-
+    }
+    void ApplyGravity()
+    {
         if (Input.GetButtonDown("Jump") && m_isGrounded)
         {
             Velocity.y += m_jumpheight;
-        } 
-        else if (!m_isGrounded && isGliding) {
+        }
+        else if (!m_isGrounded && isGliding)
+        {
             //We are not aiming for a exponential fall,
             //but a constant one
             m_freeLook.m_XAxis.m_MaxSpeed = 450;
             Velocity.y += (m_gravity / 4) * Time.deltaTime;
         }
-        else if(!m_isGrounded)
+        else if (!m_isGrounded)
         {
             Velocity.y += m_gravity * Time.deltaTime;
             m_freeLook.m_XAxis.m_MaxSpeed = 0;
 
         }
 
-        //Debug.Log(Velocity.y);
-
         m_controller.Move(Velocity * Time.deltaTime);
     }
 
-   
 
     private void OnCollisionEnter(Collision collision)
     {
