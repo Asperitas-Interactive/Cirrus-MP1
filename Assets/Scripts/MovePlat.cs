d296e145253cae5db450cef1d7b1db48ef4f6e1e
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-06-24 20:22:11 +1200

    more responsive moving plat

--- Assets/Scripts/MovePlat.cs
+++ Assets/Scripts/MovePlat.cs
@@ -18,10 +18,15 @@ public class MovePlat : MonoBehaviour
     private Vector3 m_destinationMax;
     private Vector3 m_destinationMin;
 
+    private Rigidbody m_rigidBody;
+
+    bool reachmax = false;
+    bool reachmin = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_rigidBody = GetComponent<Rigidbody>();
         m_origin = transform.position;
         m_destinationMax = transform.position + m_displacementPos;
         m_destinationMin = transform.position + m_displacementNeg;
@@ -29,20 +34,16 @@ public class MovePlat : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-        
         //Move towards location
-        transform.Translate(m_vectorSpeed * Time.deltaTime, Space.World);
+        m_rigidBody.MovePosition(transform.position + m_vectorSpeed * Time.fixedDeltaTime);
         //Clamp between for correct calculation
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, m_destinationMin.x, m_destinationMax.x),
             Mathf.Clamp(transform.position.y, m_destinationMin.y, m_destinationMax.y),
             Mathf.Clamp(transform.position.z, m_destinationMin.z, m_destinationMax.z));
         //check if reached
         DestinationReach();
-        
-       
     }
 
     public void ResetPosition()
@@ -59,14 +60,18 @@ public class MovePlat : MonoBehaviour
     //Check if it reached its destination
     void DestinationReach()
     {
-        if(transform.position.x == m_destinationMax.x && transform.position.y == m_destinationMax.y && transform.position.z == m_destinationMax.z)
+        if(transform.position.x == m_destinationMax.x && transform.position.y == m_destinationMax.y && transform.position.z == m_destinationMax.z && !reachmax)
         {
             m_vectorSpeed = -m_vectorSpeed;
+            reachmax = true;
+            reachmin = false;
         }
 
-        if (transform.position.x == m_destinationMin.x && transform.position.y == m_destinationMin.y && transform.position.z == m_destinationMin.z)
+        if (transform.position.x == m_destinationMin.x && transform.position.y == m_destinationMin.y && transform.position.z == m_destinationMin.z && !reachmin)
         {
             m_vectorSpeed = -m_vectorSpeed;
+            reachmin = true;
+            reachmax = false;
         }
     }
 }
