188fdb6e908924981b691fd579924d3adf32c2e9
Pikaboi <braden.cleur@gmail.com>
2021-06-30 12:51:46 +1200

    yay pause menu better

--- Assets/Scripts/UI/OptionContentButton.cs
+++ Assets/Scripts/UI/OptionContentButton.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionContentButton : MonoBehaviour
 {
     public GameObject[] OptionContent;
 
+    [SerializeField] public Image m_Pause;
+    [SerializeField] public Image m_Play;
+
+    public void Start()
+    {
+        m_Pause.enabled = false;
+        m_Play.enabled = true;
+    }
+
     public void Update()
     {
-        if (Input.GetKeyDown("escape"))
+        if (Input.GetButtonDown("Pause"))
         {
-            Debug.Log("a");
+            onClick();
         }
     }
 
@@ -20,5 +30,8 @@ public class OptionContentButton : MonoBehaviour
         {
             go.SetActive(!go.activeSelf);
         }
+
+        m_Pause.enabled = !m_Pause.enabled;
+        m_Play.enabled = !m_Play.enabled;
     }
 }
